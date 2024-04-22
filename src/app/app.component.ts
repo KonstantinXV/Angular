00cedfa56253b9b44ed13705e0205129269016f2
KonstantinXV <lominadze.konstantine@gmail.com>
2024-04-22 19:12:53 +0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './shared/user.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular5';
+
+
+  // public name!:String;
+  // public Age!:String;
+  // public position!:String;
+  // public image!:String;
+
+public user1:User = new User();
+public userList:User[] = [];
+  public isTrue:boolean = false;
+
+  trigger(){
+    this.isTrue = true;
+    this.userList.push(this.user1)
+  }
+  
 }
---
+++ src/app/shared/user.model.ts
@@ -0,0 +1,8 @@
+export class User {
+    public name!:String;
+    public Age!:String;
+    public position!:String;
+    public image!:String;
+    public social!:String;
+
+}
